7c4c3e03a44ffc24f3acc66a5b621f32ecd0ec48
Nick Shvets <nick.shvets.dev@gmail.com>
2023-06-29 12:09:39 +0300

    added more stats

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,6 +1,22 @@
 import React from "react";
+import cookie from "./cookie.png";
+
 import "./Stats.scss";
 
 export default function Stats({ cookieCount, buildingState }) {
-  return <div className="stats">{cookieCount}</div>;
+  return (
+    <div className="stats-container">
+      <div className="stats-container__cookie-container">
+        <div className="stats">You have {cookieCount} cookies!!!</div>
+        <img
+          style={{ height: "20px", animation: "rotation 1s infinite linear" }}
+          src={cookie}
+          alt="Cookie"
+        />
+      </div>
+      <div className="stats-container__building-container">
+        You have {buildingState.buildings.length} bakeries!!!
+      </div>
+    </div>
+  );
 }
